c4dc0ca587cd98ad0971287f81ed6bd158b79094
dogyeong <shroad1802@naver.com>
2020-06-07 13:49:48 +0900

    fix bug

---
+++ docs/_next/static/PKqnbv1ZrzMweHYpXpo_E/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/_error":[a],"/about":[a],"/blog":[a],"/hireme":[a],"/index":[a],"/post":[a]}}("static\u002Fchunks\u002F66367458d10ec11e78cfb2fc4322d17fdd871d23.5e779defcfa84e4a4ded.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/xg_BYBzVJeN3x8tIrjMvl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/_error":[a],"/about":[a],"/blog":[a],"/hireme":[a],"/index":[a],"/post":[a]}}("static\u002Fchunks\u002Fd1a5f5e0e2c803f66dde60b29f7dd36e41257b0f.5e779defcfa84e4a4ded.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
